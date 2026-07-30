3051b7b35463ef602af21c3ac3a340c1d3849205
gurdip <gurdip@TeamCity>
2026-07-30 10:42:48 -0700

    parameters of 'Build' build configuration were updated (TeamCity change in 'Root / devops-gurdip / devops-gurdip-portfolio' project)

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -16,7 +16,7 @@ changeBuildType(RelativeId("Build")) {
         add {
             hashiCorpVaultParameter {
                 name = "OCTOPUS_KEY"
-                query = "S"
+                query = "kv/data/OCTOPUS_API_KEY!/key"
                 vaultId = "Vault"
                 param("buildTypeId", "DevopsGurdip_DevopsGurdipPortfolio_Build")
             }
